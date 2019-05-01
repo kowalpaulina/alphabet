9834e50d2987dbe7cf2e12f8bd629ef5bb18607e
kowalpaulina <kowalpaulina@gmail.com>
2019-05-01 20:37:02 +0200

    Add core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AComponentComponent } from './a-component/a-component.component';
+import { BComponentComponent } from './b-component/b-component.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponentComponent,
+    BComponentComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
